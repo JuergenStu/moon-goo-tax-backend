b94d8c027cd486926fa87162a58a6ddf934c6e6d
Juergen <juergen.stuermer@liwest.at>
2021-03-19 19:00:25 +0100

    - save fine to history

--- src/main/java/valkyrie/moon/goo/tax/DebtManager.java
+++ src/main/java/valkyrie/moon/goo/tax/DebtManager.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import valkyrie.moon.goo.tax.api.CharacterViewProcessor;
+import valkyrie.moon.goo.tax.api.MiningHistoryView;
+import valkyrie.moon.goo.tax.api.MiningHistoryViewRepository;
 import valkyrie.moon.goo.tax.character.Character;
 import valkyrie.moon.goo.tax.character.CharacterRepository;
 import valkyrie.moon.goo.tax.corp.wallet.TransactionLog;
@@ -25,11 +27,13 @@ public class DebtManager {
 	private TransactionLogRepository transactionLogRepository;
 	@Autowired
 	private CharacterViewProcessor characterViewProcessor;
+	@Autowired
+	private MiningHistoryViewRepository miningHistoryViewRepository;
 
 	public void increaseDebtIfNecessary() {
 		List<Character> characters = characterRepository.findAll();
 		for (Character character : characters) {
-			if (character.getDept().getToPay() > 0) {
+			if (character.getDept().getToPay() > 100_000L) {
 				TransactionLog transactionLog = transactionLogRepository.findFirstByCharacterNameOrderByTransactionDateDesc(character.getName());
 				if (transactionLog == null) {
 					continue;
@@ -47,7 +51,13 @@ public class DebtManager {
 		if (transactionDate.before(todayMinusTwoWeeks)) {
 			// increase by 10%
 			LOG.info("Increasing debt by 10% for {}.", character.getName());
-			character.getDept().setToPay((character.getDept().getToPay() / 100) * 110);
+			long initialAmount = character.getDept().getToPay();
+			long toPay = (initialAmount / 100) * 110;
+			long additionalFine = toPay - initialAmount;
+
+			character.getDept().setToPay(toPay);
+			miningHistoryViewRepository.save(
+					new MiningHistoryView(character.getName(), "Fine", 0, new Date(), "Fine", additionalFine));
 		}
 		return character;
 	}
--- src/test/java/valkyrie/moon/goo/tax/DebtManagerTest.java
+++ src/test/java/valkyrie/moon/goo/tax/DebtManagerTest.java
@@ -42,4 +42,25 @@ class DebtManagerTest {
 
 		assertThat(result.getDept().getToPay()).isEqualTo(100L);
 	}
+
+	@Test
+	void increaseDebtMultipleTimes() {
+		DebtManager debtManager = new DebtManager();
+
+		Debt debt = new Debt(1, 0L, 100_000L, new Date(1609455600000L));
+
+		Character character = new Character();
+		character.setDept(debt);
+
+		TransactionLog log = new TransactionLog("test", "test", 1.0, new Date(1609455600000L));
+		Character result = debtManager.increaseDebtIfNecessary(character, log);
+
+		assertThat(result.getDept().getToPay()).isEqualTo(110_000L);
+
+		result = debtManager.increaseDebtIfNecessary(result, log);
+		assertThat(result.getDept().getToPay()).isEqualTo(121000L);
+
+		result = debtManager.increaseDebtIfNecessary(result, log);
+		assertThat(result.getDept().getToPay()).isEqualTo(133100L);
+	}
 }
